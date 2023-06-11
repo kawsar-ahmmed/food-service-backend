0539e40d053f1dfc6818f9eb1ed708291dab1128
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-12 03:56:50 +0600

    get single user finded

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = process.env.PORT || express()
+const express = require('express');
+const app = process.env.PORT || express();
 const port = 5001;
+
 // Database MongoDb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ClientSession, ObjectId } = require('mongodb');
 
 // Midleware
 var cors = require('cors')
@@ -10,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 //
 
-const uri = "mongodb+srv://ridoy91221:GfQczLDeaODS8iSd@cluster-new.jt669nt.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ridoy91221:xoqnp8w26WarMMag@cluster0.4mncda8.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,23 +23,50 @@ const client = new MongoClient(uri, {
 // 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("You successfully connected to MongoDB!");
+        const userCollection = client.db("Services").collection("Service");
+
+        // POST user from Front end
+
+        app.post('/user', async (req, res) => {
+            const newUser = req.body; // Use req.body instead of res.body
+            console.log(newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send({result:'success'}); // Access the insertedId property of the result
+        });
+
+        // Get Data from backend
+        app.get('/user', async(req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const user = await cursor.toArray()
+            res.send(user);
+        })
+        // Find a single user
+        app.get('/user/:id' , async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result =  await userCollection.findOne(query);
+            res.send(result);
+        })
+
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
+    // new user added form front end 
+
 }
 run().catch(console.dir);
 
+
+
+
+
 // 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
+    res.send('Home page');
+});
 app.listen(port, () => {
-    console.log('Listening', port)
+    console.log('CRUD Server Running', port)
 })
